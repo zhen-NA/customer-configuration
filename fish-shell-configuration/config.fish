308aba37518ef901e68fd3f8d24535baf7959988
NiuAo <isniuao@gmail.com>
2025-01-05 22:42:55 +0800

    fish shell 相关配置

---
+++ fish-shell-configuration/config.fish
@@ -0,0 +1,10 @@
+if not status is-interactive
+    return
+end
+
+# Commands to run in interactive sessions can go here
+set -x fish_greeting "" # Disable fish greeting
+
+abbr cls clear
+
+
---
+++ fish-shell-configuration/functions/fish_prompt.fish
@@ -0,0 +1,3 @@
+function fish_prompt --description 'Write out the prompt'
+    customer_fish_prompt
+end
