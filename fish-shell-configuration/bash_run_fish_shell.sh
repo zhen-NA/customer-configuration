b5e9e2e56c563f80977f7360eb90959738513f68
NiuAo <isniuao@gmail.com>
2025-01-05 22:47:30 +0800

    bash 启动 fish shell 作为交互式 shell 相关配置

---
+++ fish-shell-configuration/bash_run_fish_shell.sh
@@ -0,0 +1,8 @@
+# add this to ~/.bashrc
+
+if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ${BASH_EXECUTION_STRING} && ${SHLVL} == 1 ]]
+then
+	shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
+	exec fish $LOGIN_OPTION
+fi
+
